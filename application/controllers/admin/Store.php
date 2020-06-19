356483228f3bd89a18266e0584c0940dfccdaceb
iqbalalam <iqbal.alam59@gmail.com>
2020-06-19 16:39:41 +0530

    update validation

--- application/controllers/admin/Store.php
+++ application/controllers/admin/Store.php
@@ -48,7 +48,7 @@ class Store extends CI_Controller{
         $this->form_validation->set_rules('paytm_mid1','Paytm MID1','required|is_unique[stores.paytm_mid1]');
         $this->form_validation->set_rules('paytm_mid2','Paytm MID2','is_unique[stores.paytm_mid2]');
         $this->form_validation->set_rules('paytm_mid3','Paytm MID3','is_unique[stores.paytm_mid3]');
-        $this->form_validation->set_rules('email_id','Email ID','required|valid_email|is_unique[stores.email_id]');
+       // $this->form_validation->set_rules('email_id','Email ID','required|valid_email|is_unique[stores.email_id]');
     
         if($this->form_validation->run())     
         {   
