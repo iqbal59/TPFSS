f004e6f04905b7dd7f2cf65fe4ec25daa20564ec
iqbalalam <iqbal.alam59@gmail.com>
2021-01-09 18:57:43 +0530

    Material Bill format changed

--- application/controllers/admin/Import.php
+++ application/controllers/admin/Import.php
@@ -208,13 +208,13 @@ class Import extends CI_Controller{
                         $row=0;
                         while(($filesop = fgetcsv($handle, 10000, ",")) !== false)
                         {
-                           if($row++ < 7 ) 
+                           if($row++ < 1 ) 
                            continue;
                          //print_r($filesop);
                           $data['amount'] = trim($filesop[5], "'");
                           $data['invoice_no'] = trim($filesop[3], "'");
                           $data['invoice_date'] = date('Y-m-d', strtotime($filesop[0]));
-                          $data['store_crm_code'] = trim($filesop[30], "'");
+                          $data['store_crm_code'] = trim($filesop[6], "'");
                           
                          
                         $this->common_model->insert_ignore($data,'material_invoices');
