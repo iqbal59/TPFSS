874172dad021ecd4081935a5dab14bb60ea812f8
iqbalalam <iqbal.alam59@gmail.com>
2021-08-16 14:52:42 +0530

    Debug email

--- application/models/Accounts_model.php
+++ application/models/Accounts_model.php
@@ -64,7 +64,8 @@ class Accounts_model extends CI_Model
 
     public function get_invoice_by_store($store_id, $desc)
     {
-        return $this->db->query("select * from invoices where store_id='".$store_id."' and descriptions like '".$desc."%'")->result_array();
+        echo $sql="select * from invoices where store_id='".$store_id."' and descriptions like '".$desc."%'";
+        return $this->db->query($sql)->result_array();
     }
 
     public function get_invoice_by_id($id)
