f8b66f02c8cfbb76a3290cace9b69789e7ba54a4
iqbalalam <iqbal.alam59@gmail.com>
2021-10-29 10:47:08 +0530

    Address changed in ledger

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -819,12 +819,10 @@ class Accounts extends CI_Controller
             <div align="right" style="font-size:9px;">
                 <b class="title_name">TUMBLEDRY SOLUTIONS PRIVATE LIMITED</b><br />
 
-                5, 512-B, 98-MODI TOWER, NEHRU PLACE, NEW DELHI,<br>
-                South East Delhi, New Delhi, Delhi 110019<br>
-                01204317564<br>
-                GSTIN 07AAHCT2140E1ZP<br>
-                PAN AAHCT2140E<br>
-                CIN U74999DL2019PTC347046
+                FF-42, Gardenia Glory, Sector 46, Noida,<br>
+                Gautam Buddha Nagar, Uttar Pradesh, 201301<br>
+                GSTIN  09AAHCT2140E1ZL<br>
+                PAN AAHCT2140E
             </div>
             <hr />
 
