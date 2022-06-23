661a1aed3ac14a66cefe6ce1d9f89302e6a37eae
iqbal <iqbal.alam59@gmail.com>
2022-06-23 18:59:45 +0530

    mail content

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -152,8 +152,8 @@ class Accounts extends CI_Controller
                 $message.="<br>To Pay your pending balance, click <a href='https://simplifytumbledry.in/payment/pay/".base64_encode($storeData['id'])."'>https://simplifytumbledry.in/payment/pay/".base64_encode($storeData['id'])."</a><br>";
             }
          
-            $message.='<br>To make payments and check FSS statements, account statements, ledger, invoices, you can login our centralized portal "Simplify Tumbledry" using link:  <a href="https://simplifytumbledry.in/
-                ">https://simplifytumbledry.in</a> <br><br><br><p>Regards<br><br>Thanks<br><a href="mailto:mis@tumbledry.in">mis@tumbledry.in</a></p>';
+            // $message.='<br>To make payments and check FSS statements, account statements, ledger, invoices, you can login our centralized portal "Simplify Tumbledry" using link:  <a href="https://simplifytumbledry.in/
+            //     ">https://simplifytumbledry.in</a> <br><br><br><p>Regards<br><br>Thanks<br><a href="mailto:mis@tumbledry.in">mis@tumbledry.in</a></p>';
            
            
             $subject=$storeData['firm_name']."-Financial Settlement Sheet for the period ".$invoiceData->descriptions;
