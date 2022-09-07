2fffbbf6e4a8ae3a5b79f239dccc9e43ea910863
iqbal <iqbal.alam59@gmail.com>
2022-09-07 17:41:27 +0530

    api invoice no

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -3,7 +3,7 @@ class Api_model extends CI_Model
 {
     public function get_all_invoice()
     {
-        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, invoices.invoice_no");
+        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, concat('TD/','22-23','/', invoices.invoice_no) as invoice_no");
         $this->db->from("invoices");
         $this->db->join("stores", "stores.id=invoices.store_id", "left");
         $this->db->where('is_sync', 0);
