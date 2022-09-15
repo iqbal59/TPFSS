c0245795e659c75e96458582764256d54b37b7bc
iqbal <iqbal.alam59@gmail.com>
2022-09-15 16:08:45 +0530

    Tally Api updated

--- application/controllers/v1/Api.php
+++ application/controllers/v1/Api.php
@@ -17,8 +17,85 @@ class Api extends REST_Controller
 public function invoices_get()
 {
     $invoices = array();
-    $invoices = $this->api_model->get_all_invoice();
-    $this->set_response($invoices, REST_Controller::HTTP_OK);
+    $items = $this->api_model->get_all_invoice();
+
+    foreach ($items as $item) {
+        $invoiceItem['id']=$item->id;
+        $invoiceItem['voucher_type']='Sales';
+        $invoiceItem['voucher_no']=$item->invoice_no;
+        $invoiceItem['voucher_date']=date('d/m/Y', strtotime($item->invoice_date));
+        $invoiceItem['party_name']=$item->firm_name;
+        $invoiceItem['address1']=$item->store_address;
+        $invoiceItem['address2']=$item->id;
+        $invoiceItem['address3']=$item->store_city;
+        $invoiceItem['state']=$item->store_state;
+        $invoiceItem['country']='India';
+        $invoiceItem['gst_no']=$item->gstin_no;
+        $invoiceItem['pan_no']=$item->pan_no;
+        $invoiceItem['pin_code']='';
+        $invoiceItem['email_id']=$item->email_id;
+        $invoiceItem['reg_type']=$item->gstin_no != ''?"Regular":"Non Regsitered";
+        $invoiceItem['contact_person']='';
+        $invoiceItem['mobile_No']=$item->contact_number;
+        $invoiceItem['narration']='';
+        $ledgerDetails=array();
+        $ld['ledger_name']=$item->firm_name;
+        $ld['ledger_perc']="";
+        $ld['ledger_amt']=$item->net_amount;
+        $ld['dr_cr']="DR";
+        array_push($ledgerDetails, $ld);
+
+        $ld['ledger_name']="Royalty on Laundry";
+        $ld['ledger_perc']="";
+        $ld['ledger_amt']=$item->amount;
+        $ld['dr_cr']="CR";
+        array_push($ledgerDetails, $ld);
+
+        if ($item->gst_st_code=='09' || $item->gst_st_code=='9') {
+            $cgstRate=$item->tax_rate/2;
+            $sgstRate=$item->tax_rate/2;
+            $igstRate='0.00';
+            $cgstAmount=$item->tax_amount/2;
+            $sgstAmount=$item->tax_amount/2;
+            $igstAmount='0.00';
+        } else {
+            $cgstRate='0.00';
+            $sgstRate='0.00';
+            $igstRate=$item->tax_rate;
+            $cgstAmount='0.00';
+            $sgstAmount='0.00';
+            $igstAmount=$item->tax_amount;
+        }
+
+
+        $ld['ledger_name']="CGST";
+        $ld['ledger_perc']=$cgstRate;
+        $ld['ledger_amt']=$cgstAmount;
+        $ld['dr_cr']="CR";
+        array_push($ledgerDetails, $ld);
+
+        $ld['ledger_name']="SGST";
+        $ld['ledger_perc']=$sgstRate;
+        $ld['ledger_amt']=$sgstAmount;
+        $ld['dr_cr']="CR";
+        array_push($ledgerDetails, $ld);
+
+        $ld['ledger_name']="IGST";
+        $ld['ledger_perc']=$igstRate;
+        $ld['ledger_amt']=$igstAmount;
+        $ld['dr_cr']="CR";
+        array_push($ledgerDetails, $ld);
+
+
+        $invoiceItem['ledger_details']=$ledgerDetails;
+        array_push($invoices, $invoiceItem);
+    }
+
+
+
+
+    $response['result']=$invoices;
+    $this->set_response($response, REST_Controller::HTTP_OK);
 }
 
 
--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -3,12 +3,12 @@ class Api_model extends CI_Model
 {
     public function get_all_invoice()
     {
-        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, concat('TD/','22-23','/', invoices.invoice_no) as invoice_no");
+        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, concat('TD/','22-23','/', invoices.invoice_no) as invoice_no, store_address, store_city, store_state, gstin_no, email_id, pan_no, contact_number, amount, tax_amount, tax_rate, gst_st_code");
         $this->db->from("invoices");
         $this->db->join("stores", "stores.id=invoices.store_id", "left");
         $this->db->where('is_sync', 0);
         //$this->db->where('date(invoices.invoice_date) <=', $to_dt);
-        return $this->db->get()->result_array();
+        return $this->db->get()->result();
     }
 
 
