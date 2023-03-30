458a396b5d0f6a7917c55734118b69bb439a7c4e
Iqbal Alam <iqbalalam@Iqbals-MacBook-Pro.local>
2023-03-30 13:12:44 +0530

    refund adj

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -101,7 +101,7 @@ class Accounts extends CI_Controller
 
     public function refundAdjust()
     {
-        $this->Common_model->refundAdjust('2023-03-06', '2023-03-12');
+        $this->Common_model->refundAdjust('2023-03-06', '2023-03-19');
         $data['main_content'] = $this->load->view('admin/accounts/refund', null, true);
         $this->load->view('admin/index', $data);
     }
