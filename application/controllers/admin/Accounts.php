b600f62a35d9a8662bd170ec54057acd77d06942
Iqbal Alam <iqbal.alam59@gmail.com>
2023-06-16 11:39:19 +0530

    fin year change

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -101,7 +101,7 @@ class Accounts extends CI_Controller
 
     public function refundAdjust()
     {
-        $this->Common_model->refundAdjust('2023-05-22', '2023-05-28');
+        $this->Common_model->refundAdjust('2023-05-29', '2023-06-04');
         $data['main_content'] = $this->load->view('admin/accounts/refund', null, true);
         $this->load->view('admin/index', $data);
     }
--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -3,7 +3,7 @@ class Api_model extends CI_Model
 {
     public function get_all_invoice()
     {
-        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, concat('TD/','22-23','/', invoices.invoice_no) as invoice_no, store_address, store_city, store_state, gstin_no, email_id, pan_no, contact_number, amount, tax_amount, tax_rate, gst_st_code, 	descriptions,pin_code");
+        $this->db->select("invoices.id, net_amount, store_name, invoice_date, firm_name, store_state, concat('TD/','23-24','/', invoices.invoice_no) as invoice_no, store_address, store_city, store_state, gstin_no, email_id, pan_no, contact_number, amount, tax_amount, tax_rate, gst_st_code, 	descriptions,pin_code");
         $this->db->from("invoices");
         $this->db->join("stores", "stores.id=invoices.store_id", "left");
         $this->db->where('is_sync', 0);
@@ -16,6 +16,6 @@ class Api_model extends CI_Model
     public function sync_with_tally($invoiceId)
     {
         $this->db->where('id', $invoiceId);
-        return $this->db->update('invoices', array('is_sync'=>1));
+        return $this->db->update('invoices', array('is_sync' => 1));
     }
 }
