5233748cecb49169967e6ef98616e377ed5c0803
Iqbal Alam <iqbal.alam59@gmail.com>
2023-08-28 11:37:23 +0530

    paytm api udpate

---
+++ application/controllers/Api.php
@@ -0,0 +1,70 @@
+<?php
+require APPPATH . 'libraries/REST_Controller.php';
+
+
+
+class Api extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('api_model');
+        $this->load->model('Voucher_model');
+        $this->load->model('store_model');
+        $this->load->library('form_validation');
+        header('Content-Type: application/json');
+    }
+
+
+    //Start Paytm Payment
+
+    public function paytm_payment_post()
+    {
+        try {
+            //$_POST = json_decode(file_get_contents('php://input'), true);
+            $data = file_get_contents('php://input');
+            // $this->form_validation->set_rules('order_id', 'Order ID', 'trim|required');
+            // $this->form_validation->set_rules('amount', 'Amount', 'trim|required');
+
+
+            // if (!$this->form_validation->run()) {
+            //     throw new Exception(validation_errors());
+            // }
+
+
+            // $storeCode = current(explode("_", $this->input->post('order_id')));
+
+            // $storeInfo = $this->store_model->get_store_by_code($storeCode);
+
+
+            $data = array(
+
+                'descriptions' => $data
+            );
+
+
+
+            if ($this->Voucher_model->add_paytm_raw($data) > 0) {
+                $this->set_response([
+                    'status' => true,
+                    'message' => $data,
+                ], REST_Controller::HTTP_OK);
+            } else {
+                // echo $this->db->last_query();
+                throw new Exception('Insert fail');
+            }
+        } catch (Throwable $e) {
+            $this->set_response([
+                'status' => false,
+                'message' => $e->getMessage(),
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+    //END Paytm Payment
+
+
+
+
+
+
+}
