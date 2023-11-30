b3af0b15bb843469c6c4d3b559db799a42d37550
iqbal <iqbal.alam59@gmail.com>
2023-11-30 14:52:04 +0530

    pr udpate

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -100,7 +100,7 @@ class Accounts extends CI_Controller
 
     public function refundAdjust()
     {
-        $this->Common_model->refundAdjust('2023-11-06', '2023-11-12');
+        $this->Common_model->refundAdjust('2023-11-13', '2023-11-19');
         $data['main_content'] = $this->load->view('admin/accounts/refund', null, true);
         $this->load->view('admin/index', $data);
     }
