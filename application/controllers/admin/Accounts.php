02821314155ca01422ed6cc2f820f8da9ef45814
iqbal <iqbal.alam59@gmail.com>
2023-12-07 14:36:42 +0530

    pr u

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -100,7 +100,7 @@ class Accounts extends CI_Controller
 
     public function refundAdjust()
     {
-        $this->Common_model->refundAdjust('2023-11-13', '2023-11-19');
+        $this->Common_model->refundAdjust('2023-11-20', '2023-11-26');
         $data['main_content'] = $this->load->view('admin/accounts/refund', null, true);
         $this->load->view('admin/index', $data);
     }
