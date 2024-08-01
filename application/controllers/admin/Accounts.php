76cb2b8f8a2de92128867d201ba29d6ef4d0d6e6
iqbal <iqbal.alam59@gmail.com>
2024-08-01 10:31:47 +0530

    update

--- application/controllers/admin/Accounts.php
+++ application/controllers/admin/Accounts.php
@@ -100,7 +100,7 @@ class Accounts extends CI_Controller
 
     public function refundAdjust()
     {
-        $this->Common_model->refundAdjust('2024-07-08', '2024-07-14');
+        $this->Common_model->refundAdjust('2024-07-15', '2024-07-21');
         $data['main_content'] = $this->load->view('admin/accounts/refund', null, true);
         $this->load->view('admin/index', $data);
     }
